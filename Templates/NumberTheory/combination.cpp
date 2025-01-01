6f71fc2d8b9e8b30d0636eabff42cc2ce316e2a5
Hayford Akomeah <hayford@mit.edu>
2025-01-01 12:06:37 -0500

    Update with add and mul to comb

--- Templates/NumberTheory/combination.cpp
+++ Templates/NumberTheory/combination.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 #define int int64_t
-
 struct Comb {
   vector<long long> fact, invFact;
   long long mod;
 
-  Comb(int n, int mod = 1e9 + 7) : fact(n + 1), invFact(n + 1), mod(mod) {
+  Comb(int n, long long mod = 1e9 + 7) : fact(n + 1), invFact(n + 1), mod(mod) {
     fact[0] = 1;
     for (int i = 1; i <= n; i++) {
       fact[i] = (fact[i - 1] * i) % mod;
@@ -19,7 +18,7 @@ struct Comb {
     }
   }
 
-  long long pow_mod(long long a, long long b) {
+  inline long long pow_mod(long long a, long long b, long long mod = 1e9 + 7) {
     if (b < 0) {
       return pow_mod(pow_mod(a, -b), mod - 2);
     }
@@ -35,10 +34,26 @@ struct Comb {
     return res;
   }
 
-  long long nCr(int n, int r) {
+  inline long long nCr(int n, int r) {
     if (r < 0 || r > n || r < 0) {
       return 0;
     }
+    if (r == 0 || r == n) {
+      return 1;
+    }
     return (fact[n] * invFact[r] % mod) * invFact[n - r] % mod;
   }
+
+  inline int add(int a, int b) {
+    a %= mod;
+    b %= mod;
+    return (((a + b) % mod) + mod) % mod;
+  }
+
+  inline int mul(int a, int b) {
+    a %= mod;
+    b %= mod;
+    return (a * b) % mod;
+  }
 };
+#undef int
