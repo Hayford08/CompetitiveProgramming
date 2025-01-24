b6eca33b4755947d87b654da7cfe0f650cae9b6e
Hayford Akomeah <hayford@mit.edu>
2025-01-23 22:09:18 -0500

    Update for leetcode

--- Templates/NumberTheory/combination.cpp
+++ Templates/NumberTheory/combination.cpp
@@ -3,24 +3,24 @@ using namespace std;
 
 #define int int64_t
 constexpr int MOD = 1e9 + 7;
-inline int add(int a, int b, int mod = MOD) {
+inline long long add(long long a, long long b, long long mod) {
   a %= mod;
   b %= mod;
   return (((a + b) % mod) + mod) % mod;
 }
 
-inline int mul(int a, int b, int mod = MOD) {
+inline long long mul(long long a, long long b, int mod) {
   a %= mod;
   b %= mod;
   return (a * b) % mod;
 }
 
-inline int pow_mod(int a, int b, int mod = MOD) {
+inline long long pow_mod(long long a, long long b, long long mod) {
   if (b < 0) {
     return pow_mod(pow_mod(a, -b, mod), mod - 2, mod);
   }
   a %= mod;
-  int res = 1;
+  long long res = 1;
   while (b > 0) {
     if (b & 1) {
       res = (res * a) % mod;
@@ -32,22 +32,22 @@ inline int pow_mod(int a, int b, int mod = MOD) {
 }
 
 struct Comb {
-  vector<int> fact, invFact;
-  int mod;
+  vector<long long> fact, invFact;
+  long long mod;
 
   Comb(int n, int mod) : fact(n + 1), invFact(n + 1), mod(mod) {
     fact[0] = 1;
     for (int i = 1; i <= n; i++) {
       fact[i] = (fact[i - 1] * i) % mod;
     }
-    int inv = pow_mod(fact[n], mod - 2, mod);
+    long long inv = pow_mod(fact[n], mod - 2, mod);
     for (int i = n; i >= 0; i--) {
       invFact[i] = inv;
       inv = (inv * i) % mod;
     }
   }
 
-  inline int nCr(int n, int r) {
+  inline long long nCr(int n, int r) {
     if (r < 0 || r > n || r < 0) {
       return 0;
     }
@@ -57,21 +57,21 @@ struct Comb {
     return mul(fact[n], mul(invFact[r], invFact[n - r], mod), mod);
   }
 
-  inline int stars_and_bars(int stars, int slots) {
+  inline long long stars_and_bars(int stars, int slots) {
     return nCr(stars + slots - 1, slots - 1); 
   }
 };
 
 
 // for r small and n large
-inline int nCr(int n, int r) {
+inline long long nCr(int n, int r) {
   if (r < 0 || r > n || r < 0) {
     return 0;
   }
   if (r == 0 || r == n) {
     return 1;
   }
-  int res = 1;
+  long long res = 1;
   for (int i = 0; i < r; i++) {
     res = mul(res, n - i, MOD);
     res = mul(res, pow_mod(i + 1, MOD - 2, MOD), MOD);
