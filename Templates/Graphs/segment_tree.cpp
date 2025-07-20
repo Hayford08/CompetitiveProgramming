be3de1eba2e5b18ac064c839159040aff5f069de
Hayford Akomeah <hayford@mit.edu>
2025-07-20 17:45:32 -0400

    Update to segment tree

--- Templates/Graphs/segment_tree.cpp
+++ Templates/Graphs/segment_tree.cpp
@@ -4,31 +4,49 @@
 
 using namespace std;
 
-template <class T>
+struct Node {
+  // TODO: Update based on the problem
+  array<int, 6> freq;
+  Node() : freq{0, 0, 0, 0, 0, 0} {}
+
+  Node(int x) : freq{0, 0, 0, 0, 0, 0} {
+    if (0 <= x && x < 6) {
+      freq[x]++;
+    }
+  }
+
+  Node operator+(const Node& other) const {
+    Node result;
+    for (int i = 0; i < 6; i++) {
+      result.freq[i] = this->freq[i] + other.freq[i];
+    }
+    return result;
+  }
+};
+
 class SegmentTree {
 private:
-  const T DEFAULT = 1e9;
+  const Node DEFAULT = Node();
   int len;
-  std::vector<T> segtree;
+  std::vector<Node> segtree;
   vector<int> arr;
 
-  // Function to build the segment tree
   void build(int tIdx, int l, int r) {
     if (l == r) {
-        segtree[tIdx] = arr[l];
-        return;
+      segtree[tIdx] = Node(arr[l]);
+      return;
     }
     int m = l + (r - l) / 2;
     int lc = tIdx + 1;
     int rc = tIdx + 2 * (m - l + 1);
     build(lc, l, m);
     build(rc, m + 1, r);
-    segtree[tIdx] = combine(segtree[lc], segtree[rc]);
+    segtree[tIdx] = segtree[lc] + segtree[rc];
   }
 
-  void point_update(int ind, T val, int tidx, int tl, int tr) {
+  void point_update(int ind, int val, int tidx, int tl, int tr) {
     if (tl == tr) {
-      segtree[tidx] = val;
+      segtree[tidx] = Node(val);
       return;
     }
     int mid = (tl + tr) / 2;
@@ -40,14 +58,10 @@ private:
     else {
       point_update(ind, val, rchild, mid + 1, tr);
     }
-    segtree[tidx] = combine(segtree[lchild], segtree[rchild]);
+    segtree[tidx] = segtree[lchild] + segtree[rchild];
   }
 
-  T combine(T left_val, T right_val){
-    return std::min(left_val, right_val);
-  }
-
-  T range_query(int l, int r, int tidx, int tl, int tr) {
+  Node range_query(int l, int r, int tidx, int tl, int tr) {
     if (l > tr || r < tl) {
       return DEFAULT;
     }
@@ -57,23 +71,23 @@ private:
     int tm = (tl + tr) / 2;
     int lchild = tidx + 1;
     int rchild = tidx + 2 * (tm - tl + 1);
-    return combine(range_query(l, min(r, tm), lchild, tl, tm),
-                   range_query(max(l, tm + 1), r, rchild, tm + 1, tr));
+    return range_query(l, min(r, tm), lchild, tl, tm) +
+           range_query(max(l, tm + 1), r, rchild, tm + 1, tr);
   }
 
 public:
   SegmentTree(int len) : len(len), segtree(len * 2 + 1, DEFAULT) {}
 
-  SegmentTree(vector<T> &values) : 
+  SegmentTree(vector<int> &values) :
     len(values.size()), segtree(2 * values.size() + 1, DEFAULT), arr(values) {
       build(0, 0, len - 1);
     }
 
-  void point_update(int ind, T val) {
+  void point_update(int ind, int val) {
     point_update(ind, val, 0, 0, len - 1);
   }
 
-  T range_query(int l, int r) {
+  Node range_query(int l, int r) {
     return range_query(l, r, 0, 0, len - 1);
   }
 };
