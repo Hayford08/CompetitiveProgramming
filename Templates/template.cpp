49eaa281f6186c086915510d288e0429d64b9f49
Hayford Akomeah <hayford@mit.edu>
2025-08-15 20:24:49 -0400

    Updated template

--- Templates/template.cpp
+++ Templates/template.cpp
@@ -4,15 +4,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct FastIO {
-  FastIO() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    cout << fixed << setprecision(15);
-  }
-} fast_io;
-
 bool multi = true;
 inline void solve(){
   int n;  
@@ -21,6 +12,10 @@ inline void solve(){
 }
 
 int main(){
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+
   int t = 1;
   if (multi) {
     cin >> t;
