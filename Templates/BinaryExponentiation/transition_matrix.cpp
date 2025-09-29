977b9c2a6903ad66489a9369339d39a680e6aa0f
Hayford Akomeah <hayford@mit.edu>
2025-09-28 23:56:40 -0400

    Matrix exponentiation

--- Templates/BinaryExponentiation/transition_matrix.cpp
+++ Templates/BinaryExponentiation/transition_matrix.cpp
@@ -5,36 +5,55 @@ using Matrix = vector<vector<int>>;
 using Vector = vector<int>;
 const long long MOD = 1e9 + 7;
 
-inline Matrix mul(const Matrix& A, const Matrix& B) {
+using Matrix = vector<vector<int>>;
+using Vector = vector<int>;
+const long long MOD = 1e9 + 7;
+
+inline void mulInPlace(const Matrix &A, const Matrix &B, Matrix &C) {
+  for (auto &v : C) {
+    ranges::fill(v, 0);
+  }
   int rows = A.size(), cols = B[0].size();
   int inner = B.size();
-  Matrix result(rows, Vector(cols, 0));
-  for (int i = 0; i < rows; ++i) {
-    for (int j = 0; j < cols; ++j) {
-      for (int k = 0; k < inner; ++k) {
-        if (A[i][k] == 0 || B[k][j] == 0) continue;
-        long long tmp = result[i][j] + 1ll * A[i][k] * B[k][j];
+  for (int i = 0; i < rows; i++) {
+    const auto &Ai = A[i];
+    auto &Ci = C[i];
+    for (int k = 0; k < inner; k++) {
+      int Aik = Ai[k];
+      if (Aik == 0) continue;
+      const auto &Bk = B[k];
+      for (int j = 0; j < cols; j++) {
+        long long tmp = Ci[j] + 1ll * Aik * Bk[j];
         if (tmp > MOD) {
-            tmp %= MOD;
+          tmp %= MOD;
         }
-        result[i][j] = tmp;
+        Ci[j] = tmp;
       }
     }
   }
+}
+
+inline Matrix mul(const Matrix& A, const Matrix& B) {
+  int rows = A.size(), cols = B[0].size();
+  Matrix result(rows, Vector(cols, 0));
+  mulInPlace(A, B, result);
   return result;
 }
 
+// Assumes T is n x n
 inline Matrix pow(Matrix T, int b) {
   int n = T.size();
   Matrix result(n, Vector(n, 0));
-  
+  Matrix tmp = result;
   for (int i = 0; i < n; ++i) result[i][i] = 1;
 
   while (b > 0) {
     if (b % 2 == 1) {
-      result = mul(result, T);
+      mulInPlace(result, T, tmp);
+      result.swap(tmp);
     }
-    T = mul(T, T);
+    mulInPlace(T, T, tmp);
+    T.swap(tmp);
     b >>= 1;
   }
   return result;
