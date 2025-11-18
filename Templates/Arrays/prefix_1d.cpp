cf8b051a42231dbcc227c9bab0295050dac39d3a
Hayford Akomeah <hayford@mit.edu>
2025-11-17 21:53:25 -0500

    Update to templated prefix

--- Templates/Arrays/prefix_1d.cpp
+++ Templates/Arrays/prefix_1d.cpp
@@ -27,27 +27,54 @@ using namespace std;
 //   }
 // };
 
-
+// 1-based indexing
+template <typename T>
 struct Prefix {
-  vector<long long> pref;
-  
-  Prefix(vector<int> &arr) {
+  vector<T> pref;
+
+  Prefix() {}
+
+  Prefix(const vector<T> &arr) {
     int n = arr.size();
-    pref = vector<long long>(n, arr[0]);
-    for (int i = 1; i < n; i++) {
-      pref[i] = pref[i -  1] + arr[i];
+    pref.resize(n + 1);
+    pref[0] = 0;
+    for (int i = 1; i <= n; i++) {
+      pref[i] = pref[i - 1] + arr[i - 1];
     }
   }
 
-  long long query(int r) {
-    if (r < 0) return 0;
+  T query(int l, int r) {
+    if (l > r || r < 0) return T{};
+    return pref[r] - (l > 0 ? pref[l - 1] : 0);
+  }
+
+  T query(int r) {
+    if (r < 0) return T{};
     return pref[r];
   }
-
-  long long query(int l, int r) {
-    if (l > r || r < 0) {
-      return 0;
-    }
-    return l <= 0 ? pref[r] : pref[r] - pref[l - 1];
-  }
 };
+
+
+// struct Prefix {
+//   vector<long long> pref;
+  
+//   Prefix(vector<int> &arr) {
+//     int n = arr.size();
+//     pref = vector<long long>(n, arr[0]);
+//     for (int i = 1; i < n; i++) {
+//       pref[i] = pref[i -  1] + arr[i];
+//     }
+//   }
+
+//   long long query(int r) {
+//     if (r < 0) return 0;
+//     return pref[r];
+//   }
+
+//   long long query(int l, int r) {
+//     if (l > r || r < 0) {
+//       return 0;
+//     }
+//     return l <= 0 ? pref[r] : pref[r] - pref[l - 1];
+//   }
+// };
